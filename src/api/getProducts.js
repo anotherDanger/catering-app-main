be7bcb5978c47d6ded589f1d5fe20d7473135189
Andhika Danger <andhikadanger666@gmail.com>
2025-06-02 08:59:02 +0700

    api

---
+++ src/api/getProducts.js
@@ -0,0 +1,23 @@
+import { data } from "react-router-dom";
+
+async function getProducts(){
+    const request = new Request("http://localhost:8080/v1/products", {
+        headers: {
+            "Content-Type":"application/json"
+        },
+        method:"GET"
+    });
+
+    try{
+        const data = await fetch(request);
+    if (! data.ok){
+     throw new(data.statusText)   
+    }
+    const response = await data.json();
+
+    return response;
+    }catch(error){
+        console.log(error)
+    }
+
+}
