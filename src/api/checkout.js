a5688c585474357ed5d55ea47407243e313ebd7c
Andhika Danger <andhikadanger666@gmail.com>
2025-06-28 14:45:39 +0700

    checkout post

---
+++ src/api/checkout.js
@@ -0,0 +1,20 @@
+export async function postCheckout(data) {
+  const response = await fetch('http://localhost:8083/v1/checkout', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  })
+
+  if (!response.ok) {
+    const errorData = await response.json()
+    throw new Error(errorData.message || 'Error posting checkout data')
+  }
+
+  if (response.status === 204) {
+    return null
+  }
+
+  return await response.json()
+}
