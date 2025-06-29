808f98ff27942932c7a0841432d4932cde623f2a
Andhika Danger <andhikadanger666@gmail.com>
2025-06-30 05:49:59 +0700

    offcanvas history button

--- src/components/home/Navbar.jsx
+++ src/components/home/Navbar.jsx
@@ -1,45 +1,88 @@
+import React from "react";
+import OffCanvasHistory from "./OffCanvasHistory";
+import OffCanvasProfile from "./OffCanvasProfile";
+import OffCanvasCart from "./OffCanvasCart";
 
-function Navbar() {
-    return (
-        <div>
-            <nav className="navbar navbar-expand-lg navbar-transparent navbar-brown navbar-dark shadow-lg fixed-top">
-                <div className="container">
-                    <a className="navbar-brand" href="#">Khaira Catering</a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
-                            data-bs-target="#navbarText">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse text-right" id="navbarText">
-                        <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
-                            <li className="nav-item"><a className="nav-link active" href="#home">Home</a></li>
-                            <li className="nav-item"><a className="nav-link active" href="#menu">Menu</a></li>
-                            <li className="nav-item"><a className="nav-link active" href="#about">About</a></li>
-                            <li className="nav-item"><a className="nav-link active" href="#kontak">Contact</a></li>
-                        </ul>
-                        <ul className="navbar-nav">
-                            <li className="nav-item">
-                                <button
-                                    className="btn btn-outline-light"
-                                    type="button"
-                                    data-bs-toggle="offcanvas"
-                                    data-bs-target="#sidebarProfile"
-                                    aria-controls="sidebarProfile"
-                                >
-                                    <i className="fa-solid fa-user"></i> Profil
-                                </button>
-                            </li>
-                            <li className="nav-item">
-                                <button className="btn btn-cart" id="viewCartBtn" type="button"
-                                        data-bs-toggle="offcanvas" data-bs-target="#offCanfasCart">
-                                    <i className="fa-solid fa-cart-shopping fa-xl text-white"></i>
-                                </button>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
+export default function App() {
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg navbar-transparent navbar-brown navbar-dark shadow-lg fixed-top">
+        <div className="container">
+          <a className="navbar-brand" href="#">
+            Khaira Catering
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarText"
+            aria-controls="navbarText"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarText">
+            <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" href="#home">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" href="#menu">
+                  Menu
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" href="#about">
+                  About
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" href="#kontak">
+                  Contact
+                </a>
+              </li>
+            </ul>
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <button
+                  className="btn btn-outline-light"
+                  type="button"
+                  data-bs-toggle="offcanvas"
+                  data-bs-target="#offcanvasHistory"
+                  aria-controls="offcanvasHistory"
+                >
+                  <i className="fa-solid fa-clock-rotate-left"></i> Riwayat
+                </button>
+              </li>
+              <li className="nav-item">
+                <button
+                  className="btn btn-outline-light"
+                  type="button"
+                  data-bs-toggle="offcanvas"
+                  data-bs-target="#sidebarProfile"
+                  aria-controls="sidebarProfile"
+                >
+                  <i className="fa-solid fa-user"></i> Profil
+                </button>
+              </li>
+              <li className="nav-item">
+                <button
+                  className="btn btn-cart"
+                  type="button"
+                  data-bs-toggle="offcanvas"
+                  data-bs-target="#offCanfasCart"
+                  aria-controls="offCanfasCart"
+                >
+                  <i className="fa-solid fa-cart-shopping fa-xl text-white"></i>
+                </button>
+              </li>
+            </ul>
+          </div>
         </div>
-    )
+      </nav>
+    </>
+  );
 }
-
-export default Navbar;
