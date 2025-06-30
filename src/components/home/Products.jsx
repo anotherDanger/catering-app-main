54e3b2eaa9391ec1f0e42d4c728ce28ade813e8b
Andhika Danger <andhikadanger666@gmail.com>
2025-07-01 01:31:41 +0700

    swiper and carousel

--- src/components/home/Products.jsx
+++ src/components/home/Products.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState, useRef } from "react"
 import ModalProduct from "./ModalProduct"
 import getProducts from "../../api/getProducts"
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/css'
 
 function Products() {
   const [products, setProducts] = useState([])
@@ -39,12 +41,25 @@ function Products() {
       <div className="container-fluid pt-5 menu" id="menu">
         <div className="container text-center">
           <h2 className="mt-3">Produk</h2>
-          <div className="row pt-4 gx-4 gy-4">
-            {products.length === 0 ? (
-              <p>Loading...</p>
-            ) : (
-              products.map((product, index) => (
-                <div className="col-md-4" key={product.product_id || index}>
+          {products.length === 0 ? (
+            <p>Loading...</p>
+          ) : (
+            <Swiper
+              spaceBetween={20}
+              slidesPerView={1}
+              breakpoints={{
+                576: {
+                  slidesPerView: 2,
+                  spaceBetween: 20,
+                },
+                768: {
+                  slidesPerView: 3,
+                  spaceBetween: 30,
+                },
+              }}
+            >
+              {products.map((product, index) => (
+                <SwiperSlide key={product.product_id || index}>
                   <div className="card crop-img">
                     <img
                       src={product.product_image || "../img-products/sample.jpg"}
@@ -62,10 +77,10 @@ function Products() {
                       Lihat Produk
                     </button>
                   </div>
-                </div>
-              ))
-            )}
-          </div>
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          )}
         </div>
       </div>
       <ModalProduct product={selectedProduct} onClose={closeModal} modalRef={modalRef} />
