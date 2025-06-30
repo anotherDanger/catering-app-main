621e7c2b7f4b68f74cd880c32937bc399e801d51
Andhika Danger <andhikadanger666@gmail.com>
2025-06-30 17:25:40 +0700

    bearer token

--- src/api/checkout.js
+++ src/api/checkout.js
@@ -1,20 +1,23 @@
 export async function postCheckout(data) {
+  const token = localStorage.getItem('access_token');
+
   const response = await fetch('https://khatering.shop/user/api/v1/checkout', {
     method: 'POST',
     headers: {
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`
     },
     body: JSON.stringify(data)
-  })
+  });
 
   if (!response.ok) {
-    const errorData = await response.json()
-    throw new Error(errorData.message || 'Error posting checkout data')
+    const errorData = await response.json();
+    throw new Error(errorData.message || 'Error posting checkout data');
   }
 
   if (response.status === 204) {
-    return null
+    return null;
   }
 
-  return await response.json()
+  return await response.json();
 }
--- src/api/history.js
+++ src/api/history.js
@@ -1,13 +1,18 @@
 export async function fetchHistory(username) {
   try {
-    const response = await fetch("https://khatering.shop/user/v1/history/" + username);
+    const token = localStorage.getItem('access_token');
+
+    const response = await fetch(`https://khatering.shop/user/api/v1/history/${username}`, {
+      method: 'GET',
+      headers: {
+        'Authorization': `Bearer ${token}`
+      }
+    });
+
     if (!response.ok) throw new Error(response.status);
+
     const result = await response.json();
-    if (result && result.data) {
-      return result.data;
-    } else {
-      return null;
-    }
+    return result?.data ?? null;
   } catch (e) {
     return null;
   }
