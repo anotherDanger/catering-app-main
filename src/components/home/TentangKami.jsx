1422039ef94e20b26af5c8feca36214bdc03e5b8
Andhika Danger <andhikadanger666@gmail.com>
2025-07-02 05:13:42 +0700

    banner

--- src/components/home/TentangKami.jsx
+++ src/components/home/TentangKami.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import "tentangkami.css"
+import "./tentangkami.css"
 
 const TentangKami = () => {
   useEffect(() => {
