cadde616c51ad484d103b631f6e4d6cfd0bdc3fc
Andhika Danger <andhikadanger666@gmail.com>
2025-07-02 04:20:59 +0700

    banner

--- src/components/home/Banner.jsx
+++ src/components/home/Banner.jsx
@@ -1,18 +1,17 @@
-
 function Banner() {
     return (
         <div>
             <div className="container-fluid banner" id="home">
-            <div className="container">
-                <h4 className="display-1">KHAIRA CATERING</h4>
-                <h1 className="text-danger">The premium Quality</h1>
-                <h3 className="display-7 text-light">Khaira Catering Menyediakan Makanan Berkualitas</h3>
-                <a href="#menu">
-                    <button type="button" className="btn btn-order-now btn-lg mt-4">Order Now</button>
-                </a>
-            </div>
+                <div className="container">
+                    <h4 className="display-1">Khaira Catering</h4>
+                    <h1 className="text-danger">Menghidangkan Cita Rasa Mengikat Kenangan</h1>
+                    <h3 className="display-7 text-light">Harga Terjangkau</h3>
+                    <a href="#menu">
+                        <button type="button" className="btn btn-order-now btn-lg mt-4">Order Now</button>
+                    </a>
+                </div>
             </div>
         </div>
-    )
+    );
 }
 export default Banner;
