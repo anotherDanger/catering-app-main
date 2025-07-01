f9e7bd0f1960003a6b00c30d3c815a0f0fdd77e1
Andhika Danger <andhikadanger666@gmail.com>
2025-07-02 01:36:20 +0700

    static file serve

--- src/components/home/ModalProduct.jsx
+++ src/components/home/ModalProduct.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { addToCart } from '../../api/cart'
-import getProducts, { getProductImage } from '../../api/products'
+import getProducts, { getProductImage } from '../../api/getProducts.js'
 
 function ModalProduct({ product, onClose, modalRef }) {
   const navigate = useNavigate()
