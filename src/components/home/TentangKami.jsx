1d1152e5ba36dd669fc6e8251812d50e7f6e89e4
Andhika Danger <andhikadanger666@gmail.com>
2025-07-02 04:59:26 +0700

    banner

--- src/components/home/TentangKami.jsx
+++ src/components/home/TentangKami.jsx
@@ -2,9 +2,9 @@ import React, { useEffect } from 'react';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
-import img1 from '../assets/img/img1.jpg';
-import img2 from '../assets/img/img2.jpg';
-import img3 from '../assets/img/img3.jpg';
+// import img1 from '../assets/img/img1.jpg';
+// import img2 from '../assets/img/img2.jpg';
+// import img3 from '../assets/img/img3.jpg';
 
 const TentangKami = () => {
   useEffect(() => {
