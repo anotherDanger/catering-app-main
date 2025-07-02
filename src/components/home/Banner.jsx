948275ca7121d7e98404c2b8205acd98dd89d480
Andhika Danger <andhikadanger666@gmail.com>
2025-07-02 20:58:16 +0700

    media

--- src/components/home/Banner.jsx
+++ src/components/home/Banner.jsx
@@ -4,9 +4,9 @@ function Banner() {
     return (
         <div className="container-fluid banner" id="home">
             <div className="container">
-                <h4 className="display-1">Khaira Catering</h4>
-                <h1 className="text-danger">Menghidangkan Cita Rasa Mengikat Kenangan</h1>
-                <h3 className="display-7 text-light">Harga Terjangkau</h3>
+                <h4 className="display-1" style={{color: 'black'}}>Khaira Catering</h4>
+                <h1 className="text-danger" style={{color: 'red'}}>Menghidangkan Cita Rasa Mengikat Kenangan</h1>
+                <h3 className="display-7" style={{color: 'gray'}}>Harga Terjangkau</h3>
                 <a href="#menu">
                     <button type="button" className="btn btn-primary btn-lg mt-4 btn-auto">Order Now</button>
                 </a>
