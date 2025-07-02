d30911df68968bac41d174a51d377e4da869ec22
Andhika Danger <andhikadanger666@gmail.com>
2025-07-02 20:02:07 +0700

    media

--- src/components/home/Banner.jsx
+++ src/components/home/Banner.jsx
@@ -1,14 +1,15 @@
+// Banner.js
 import React from 'react';
 
 function Banner() {
     return (
         <div className="container-fluid banner" id="home">
-            <div className="container text-center text-md-start">
+            <div className="container text-center text-lg-start">
                 <h4 className="display-2">Khaira Catering</h4>
                 <h1 className="display-5 text-danger">Menghidangkan Cita Rasa Mengikat Kenangan</h1>
                 <h3 className="fw-light text-light">Harga Terjangkau</h3>
                 <a href="#menu">
-                    <button type="button" className="btn btn-primary btn-lg mt-4">Order Now</button>
+                    <button type="button" className="btn btn-primary btn-lg mt-4 btn-auto">Order Now</button>
                 </a>
             </div>
         </div>
--- src/components/home/Navbar.jsx
+++ src/components/home/Navbar.jsx
@@ -6,7 +6,7 @@ import OffCanvasCart from "./OffCanvasCart";
 export default function App() {
   return (
     <>
-      <nav className="navbar navbar-expand-lg navbar-transparent navbar-brown navbar-dark shadow-lg fixed-top">
+      <nav className="navbar navbar-expand-lg navbar-brown navbar-dark shadow-lg fixed-top">
         <div className="container">
           <a className="navbar-brand" href="#">
             Khaira Catering
@@ -25,64 +25,72 @@ export default function App() {
           <div className="collapse navbar-collapse" id="navbarText">
             <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" href="#home">
+                <a className="nav-link" href="#home">
                   Home
                 </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link active" href="#menu">
+                <a className="nav-link" href="#menu">
                   Menu
                 </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link active" href="#about">
+                <a className="nav-link" href="#about">
                   About
                 </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link active" href="#kontak">
+                <a className="nav-link" href="#kontak">
                   Contact
                 </a>
               </li>
             </ul>
-            <ul className="navbar-nav">
+            <ul className="navbar-nav flex-row gap-2">
               <li className="nav-item">
                 <button
-                  className="btn btn-outline-light"
+                  className="btn btn-outline-light btn-icon"
                   type="button"
                   data-bs-toggle="offcanvas"
                   data-bs-target="#offcanvasHistory"
                   aria-controls="offcanvasHistory"
+                  title="Riwayat"
                 >
-                  <i className="fa-solid fa-clock-rotate-left"></i> Riwayat
+                  <i className="fa-solid fa-clock-rotate-left"></i>
+                  <span className="d-lg-none ms-2">Riwayat</span>
                 </button>
               </li>
               <li className="nav-item">
                 <button
-                  className="btn btn-outline-light"
+                  className="btn btn-outline-light btn-icon"
                   type="button"
                   data-bs-toggle="offcanvas"
                   data-bs-target="#sidebarProfile"
                   aria-controls="sidebarProfile"
+                  title="Profil"
                 >
-                  <i className="fa-solid fa-user"></i> Profil
+                  <i className="fa-solid fa-user"></i>
+                  <span className="d-lg-none ms-2">Profil</span>
                 </button>
               </li>
               <li className="nav-item">
                 <button
-                  className="btn btn-cart"
+                  className="btn btn-cart btn-icon"
                   type="button"
                   data-bs-toggle="offcanvas"
                   data-bs-target="#offCanfasCart"
                   aria-controls="offCanfasCart"
+                  title="Keranjang"
                 >
-                  <i className="fa-solid fa-cart-shopping fa-xl text-white"></i>
+                  <i className="fa-solid fa-cart-shopping fa-xl"></i>
                 </button>
               </li>
             </ul>
           </div>
         </div>
       </nav>
+      <OffCanvasHistory />
+      <OffCanvasProfile />
+      <OffCanvasCart />
     </>
   );
 }
