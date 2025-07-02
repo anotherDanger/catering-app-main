237359269f3e231cc2ce5b77d40de402593062dd
Andhika Danger <andhikadanger666@gmail.com>
2025-07-02 19:47:24 +0700

    media

--- src/components/home/Banner.jsx
+++ src/components/home/Banner.jsx
@@ -1,17 +1,18 @@
+import React from 'react';
+
 function Banner() {
     return (
-        <div>
-            <div className="container-fluid banner" id="home">
-                <div className="container">
-                    <h4 className="display-1">Khaira Catering</h4>
-                    <h1 className="text-danger">Menghidangkan Cita Rasa Mengikat Kenangan</h1>
-                    <h3 className="display-7 text-light">Harga Terjangkau</h3>
-                    <a href="#menu">
-                        <button type="button" className="btn btn-order-now btn-lg mt-4">Order Now</button>
-                    </a>
-                </div>
+        <div className="container-fluid banner" id="home">
+            <div className="container text-center text-md-start">
+                <h4 className="display-2">Khaira Catering</h4>
+                <h1 className="display-5 text-danger">Menghidangkan Cita Rasa Mengikat Kenangan</h1>
+                <h3 className="fw-light text-light">Harga Terjangkau</h3>
+                <a href="#menu">
+                    <button type="button" className="btn btn-primary btn-lg mt-4">Order Now</button>
+                </a>
             </div>
         </div>
     );
 }
+
 export default Banner;
