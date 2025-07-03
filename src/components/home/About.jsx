d027cabf4cd2337fa8f280a8ac19e791798dd73b
Andhika Danger <andhikadanger666@gmail.com>
2025-07-03 15:51:43 +0700

    logo

--- src/components/home/About.jsx
+++ src/components/home/About.jsx
@@ -5,26 +5,14 @@ function About() {
         <div>
             <div className="container-fluid pt-5 pb-5 about" id="about">
                 <div className="container">
-                    {/* Judul Utama Section */}
                     <div className="row mb-5">
                         <div className="col-12 text-center text-white">
                             <h2 className="display-3">Tentang Kami</h2>
                         </div>
                     </div>
 
-                    {/* Konten Utama */}
                     <div className="row justify-content-center align-items-center">
-                        {/* Kolom Gambar (kiri) */}
-                        <div className="col-md-6 text-center mb-4 mb-md-0">
-                            <img 
-                                src="../img-products/khaira.png" 
-                                className="img-fluid img-about"
-                                alt="Tentang Khaira Catering"
-                            />
-                        </div>
-
-                        {/* Kolom Teks (kanan) */}
-                        <div className="col-md-6 text-white text-md-start">
+                        <div className="col-md-6 text-white text-md-start mb-4 mb-md-0">
                             <h3 className="mb-3">Komitmen Untuk Pengalaman Kuliner Terbaik</h3>
                             <p>
                                 Khaira Catering menyajikan layanan katering dengan komitmen untuk memberikan pengalaman
@@ -35,7 +23,6 @@ function About() {
                                 pelayanan profesional dan pengalaman bertahun-tahun.
                             </p>
                             
-                            {/* Bagian Lokasi yang Ditingkatkan */}
                             <div className="mt-4">
                                 <h4>Lokasi Kami</h4>
                                 <p>
@@ -44,6 +31,15 @@ function About() {
                                     </strong>
                                 </p>
                             </div>
+                        </div>
+
+                        <div className="col-md-6 text-center">
+                            <img
+                                src="../../public/khaira.png" 
+                                className="img-fluid rounded-3 shadow"
+                                style={{ maxWidth: '450px' }}
+                                alt="Tentang Khaira Catering"
+                            />
                         </div>
                     </div>
                 </div>
